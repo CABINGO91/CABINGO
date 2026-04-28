86ef0b8b8623f8159dd0823406082b42bad82e5a
Stefan <stefan@test.no>
2026-04-28 21:52:19 +0200

    fix ssl for render

--- backend/server.js
+++ backend/server.js
@@ -66,9 +66,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server kjører på port " + PORT);
-});const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
